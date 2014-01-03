256c5fffbbfbf49a12ac8ddedc268f8088ab3490
scott1028 <mic1028002@gmail.com>
2014-01-03 20:41:41 +0800

    add comment to manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")		# mysite/settings.py 為此 django project 的主要 configure 文件
 
     from django.core.management import execute_from_command_line
 
